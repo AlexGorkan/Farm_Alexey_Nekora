e127021595c29b2ea9c0536ea5a385540ece2712
VeronikaLars <verkotalarina@gmail.com>
2021-07-06 20:59:38 +0300

    Fire Timer
    Class work

--- Farm_Alexey_Nekora_Group_2_Project/Assets/RW/Scripts/TractorMovement.cs
+++ Farm_Alexey_Nekora_Group_2_Project/Assets/RW/Scripts/TractorMovement.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class TractorMovement : MonoBehaviour
 {
+    [Header("Fire Property")]
+    [SerializeField] private GameObject senoPrefab; //chem streliat (ssilka na object)
+    [SerializeField] private Transform spawnPoint; //tochka spawna na objekte
+    [SerializeField] private float fireRate; //chastota spawna
+    private float nextFire; 
+
+    [Header("Tractor Property")]
     [SerializeField] private float speed;
     [SerializeField] private float bounds;
-
     private float direction;
     private bool isPress;
+    //[SerializeField] private string fireMessage = "";
 
 
-   
+    private void Start()
+    {
+        nextFire = fireRate; // dlja plusovogo timera
+    }
     void Update()
     {
         if (isPress)
@@ -23,6 +33,10 @@ public class TractorMovement : MonoBehaviour
                 transform.Translate(Vector3.left * speed * direction * Time.deltaTime);
             }
         }
+
+        nextFire += Time.deltaTime; // -= Time.deltaTime; minusovij timer
+
+
 
         //if (((transform.position.x > -bounds) && (direction == 1f)) || ((transform.position.x < bounds) && (direction == -1f)))
         //{
@@ -48,7 +62,16 @@ public class TractorMovement : MonoBehaviour
 
     public void PressFire()
     {
-        Debug.Log("FIRE!");
+        if (nextFire > fireRate) // (nextFire < 0)
+        {
+            GameObject seno = Instantiate(senoPrefab, spawnPoint.position, Quaternion.identity); // senoPrefab.transform.rotation - sohrahjaet povorot objecta prefaba
+            Destroy(seno, 15f);
+
+            nextFire = 0f; // nextFire = fireRate;
+        }
+        
+        
+        
     }
 
 
